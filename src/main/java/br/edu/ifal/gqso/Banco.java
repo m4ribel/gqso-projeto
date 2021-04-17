88870765b2a590324eb343af9efd804b36d23a01
m4ribel <m4ribel.santos@gmail.com>
2021-04-17 16:20:23 +0000

    Adicionando deposito,get/set saldo na classe Banco

--- src/main/java/br/edu/ifal/gqso/Banco.java
+++ src/main/java/br/edu/ifal/gqso/Banco.java
@@ -1,5 +1,17 @@
 package br.edu.ifal.gqso;
 
 public class Banco {
+    private double saldo;
     
+    public double getSaldo() {
+        return this.saldo;
+    }
+
+    public void setSaldo(double valor) {
+        this.saldo = valor;
+    }
+
+    public double deposito(double valor){
+        return 0;
+    }
 }
