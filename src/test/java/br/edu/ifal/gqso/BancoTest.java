082d631224c2e68ffa252de4d3833585223d6845
m4ribel <m4ribel.santos@gmail.com>
2021-04-17 16:18:13 +0000

    Adicionando o metodo depositoTest no BancoTeste

--- src/test/java/br/edu/ifal/gqso/BancoTest.java
+++ src/test/java/br/edu/ifal/gqso/BancoTest.java
@@ -1,6 +1,24 @@
 package br.edu.ifal.gqso;
 
+
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+
 public class BancoTest {
-    
+    Banco banco;
+
+
+    @BeforeEach
+    public void setUp(){
+        banco = new Banco();
+       
+    }
+
+    @Test
+    public void depositoTest(){
+        
+    }
 
 }
